7394e0ad925fa16639c6831a6bb2e21dec916010
Serhii Prostakov <serhii.prostakov@altran.com>
2021-11-04 14:28:51 +0200

    update server

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const http = require('http');
 const { serverLogger } = require('./logger');
+const config = require('./config');
 
 http.createServer((req, res) => {
     res.writeHeader(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ message: 'Hello world!' }));
-}).listen(3000, serverLogger);
+}).listen(config.APP_PORT, serverLogger);

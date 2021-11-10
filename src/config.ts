d6c6b96752558d402a216601657103a380ce1786
Serhii Prostakov <serhii.prostakov@altran.com>
2021-11-10 20:34:18 +0200

    updated according to the comments of the PR

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,11 @@
 var argv = process.argv.slice(2);
-process.env.PORT = '5000';
-
 interface Config {
     APP_PORT: number,
     ENV: string,
 };
 
 const config: Config = {
-    APP_PORT: Number(process.env.PORT),
+    APP_PORT: Number(process.env.PORT) || 5000,
     ENV: argv[0]
 };
 

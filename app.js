69248729f2c8f079ba353b57c96045a4babdb090
Serhii Prostakov <serhii.prostakov@altran.com>
2021-11-10 14:48:26 +0200

    deleted unnecessary files

--- app.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const { serverLogger } = require('./logger');
-const config = require('./config');
-
-http.createServer((req, res) => {
-    res.writeHeader(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ message: 'Hello world!' }));
-}).listen(config.APP_PORT, serverLogger);
--- config.js
+++
@@ -1,9 +0,0 @@
-var argv = process.argv.slice(2);
-process.env.PORT = 3000;
-
-const config = {
-    APP_PORT: process.env.PORT,
-    ENV: argv[0]
-}
-
-module.exports = config;
